a919404080d9998a2cc707d7e16de05b499bfdef
zap-mueda <m_ueda@zappallas.com>
2018-06-19 14:52:30 +0900

    演習用に変更

--- assets/form.js
+++ assets/form.js
@@ -3,25 +3,6 @@ import Vue from 'vue/dist/vue.esm.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   new Vue({
-    el: '#form',
-    data: function() {
-      return {
-        name: '',
-        gender: 'male',
-        kind: 'kind2',
-        content: '',
-        contentBaseCount: 300
-      }
-    },
-    methods: {
-    },
-    computed: {
-      contentCount() {
-        return this.contentBaseCount - this.content.value.length
-      },
-      isInvalid() {
-        return !this.name || !this.content || this.contentCount < 0
-      }
-    }
+
   })
 });
